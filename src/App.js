257c7bb46d3c6392a9db18fb57c02b4ea4beb47a
Matan Shaviro <matanshaviro@matans-mbp-2.mynet>
2020-04-11 12:50:41 +0300

    Add Alert state and component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
 import Search from './components/users/Search';
+import Alert from './components/layout/Alert'
 import axios from 'axios';
 import './App.css';
 
@@ -9,6 +10,7 @@ class App extends Component {
   state = {
     users: [],
     loading: false,
+    alert: null
   };
 
   // Search Github users
@@ -25,6 +27,12 @@ class App extends Component {
     this.setState({ users: [], loading: false });
   };
 
+  // Set alert for invalid search
+  setAlert = (msg, type) => {
+    this.setState({ alert: { msg, type } })
+    setTimeout(() => this.setState({ alert: null }), 4000)
+  }
+
   render() {
     const { users, loading } = this.state
 
@@ -32,10 +40,12 @@ class App extends Component {
       <div className='App'>
         <Navbar />
         <div className='container'>
+          <Alert alert={this.state.alert} />
           <Search
             searchUsers={this.searchUsers}
             clearUsers={this.clearUsers}
             showClear={users.length > 0 ? true : false}
+            setAlert={this.setAlert}
           />
           <Users loading={loading} users={users} />
         </div>

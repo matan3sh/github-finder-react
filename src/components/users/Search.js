3706f62f2b553fd05324e88202ac6a1fd92ba406
Matan Shaviro <matanshaviro@matans-mbp-2.mynet>
2020-04-11 12:30:00 +0300

    Add Clear Button to clear users from state

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types'
+import PropTypes from 'prop-types';
 
 export class Search extends Component {
     state = {
@@ -7,8 +7,10 @@ export class Search extends Component {
     };
 
     static propTypes = {
-        searchUsers: PropTypes.func.isRequired
-    }
+        searchUsers: PropTypes.func.isRequired,
+        clearUsers: PropTypes.func.isRequired,
+        showClear: PropTypes.bool.isRequired,
+    };
 
     onChange = (e) => this.setState({ [e.target.name]: e.target.value });
 
@@ -19,6 +21,8 @@ export class Search extends Component {
     };
 
     render() {
+        const { showClear, clearUsers } = this.props;
+
         return (
             <div>
                 <form onSubmit={this.onSubmit} className='form'>
@@ -35,6 +39,11 @@ export class Search extends Component {
                         className='btn btn-dark btn-block'
                     />
                 </form>
+                {showClear && (
+                    <button className='btn btn-light btn-block' onClick={clearUsers}>
+                        Clear
+                    </button>
+                )}
             </div>
         );
     }

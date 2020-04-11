3cdcd815a24050a0d86fbbc5c10b2fe7590cff2e
Matan Shaviro <matanshaviro@matans-mbp-2.mynet>
2020-04-11 13:04:18 +0300

    Add React Router to app

---
+++ src/components/pages/About.js
@@ -0,0 +1,14 @@
+import React, { Fragment } from 'react';
+
+export const About = () => {
+    return (
+        <Fragment>
+            <h1>About This App</h1>
+            <p>App to Search Github users</p>
+            <p>Author: Matan Shaviro</p>
+            <p>Version: 1.0.0</p>
+        </Fragment>
+    );
+};
+
+export default About;
